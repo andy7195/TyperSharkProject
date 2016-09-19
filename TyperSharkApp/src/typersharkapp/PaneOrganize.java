bef762415cdd1a58f62a2f27d817646cd9e7cb6a
edapigo <edwardapg@hotmail.es>
2016-09-18 23:01:04 -0500

    Adicion de Pulpo, Cargar la partida. Arreglo en los niveles, puntajes, velocidade,problemas de visualizacio y numeros negativos.

--- TyperSharkApp/src/typersharkapp/PaneOrganize.java
+++ TyperSharkApp/src/typersharkapp/PaneOrganize.java
@@ -1,13 +1,13 @@
 package typersharkapp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -25,6 +25,10 @@ public class PaneOrganize
     private Reglas reglas;
     private InicioJuego inicioJuego;
     private Puntajes puntajes;
+    private File file;
+    private FileReader fr;
+    private BufferedReader br;
+    private String datos[];
     
     public PaneOrganize(Stage stageP)
     {
@@ -36,6 +40,7 @@ public class PaneOrganize
         
         this.stagePrincipal = stageP;
         
+       
         vi=new ImageView(imagen);
         vi.setFitHeight(510);
         vi.setFitWidth(900);
@@ -47,14 +52,15 @@ public class PaneOrganize
         Inicio.setStyle("-fx-background-color: blue;");
         TitulosLabel Ttot= new TitulosLabel("TYPERSHARK","-fx-font: 40 elephant",450,10,270,30, Color.DARKGRAY);
         
-        Boton iniciar= new Boton("Iniciar Partida","-fx-font: 20 century; -fx-background-color: transparent; -fx-background-radius: 30;", 200, 60, 130, 410, 10, Color.AQUA);
-        Boton regla= new Boton("Reglas","-fx-font: 20 century; -fx-background-color: transparent; -fx-background-radius: 30;", 100, 60, 330, 410, 10, Color.AQUA);
-        Boton puntaje= new Boton("Top de puntajes","-fx-font: 20 century; -fx-background-color: transparent;-fx-background-radius: 30;", 180, 60, 450, 410, 10, Color.AQUA);
-        Boton salir= new Boton("Salir","-fx-font: 20 century; -fx-background-color: transparent;-fx-background-radius: 30;", 100, 60, 660, 410, 10, Color.AQUA);
+        Boton iniciar= new Boton("Iniciar Partida","-fx-font: 20 century; -fx-background-color: transparent; -fx-background-radius: 30;", 200, 60, 80, 410, 10, Color.AQUA);
+        Boton cargar= new Boton("Cargar","-fx-font: 20 century; -fx-background-color: transparent; -fx-background-radius: 30;", 100, 60, 280, 410, 10, Color.AQUA);
+        Boton regla= new Boton("Reglas","-fx-font: 20 century; -fx-background-color: transparent; -fx-background-radius: 30;", 100, 60, 400, 410, 10, Color.AQUA);
+        Boton puntaje= new Boton("Top de puntajes","-fx-font: 20 century; -fx-background-color: transparent;-fx-background-radius: 30;", 180, 60, 520, 410, 10, Color.AQUA);
+        Boton salir= new Boton("Salir","-fx-font: 20 century; -fx-background-color: transparent;-fx-background-radius: 30;", 100, 60, 700, 410, 10, Color.AQUA);
         
         //AGREGANDO NODOS A LAS VENTANAS
         Inicio.getChildren().add(vi);
-        Inicio.getChildren().addAll(Ttot.getL(),iniciar.getBtn(),regla.getBtn(),puntaje.getBtn(),salir.getBtn());
+        Inicio.getChildren().addAll(Ttot.getL(),iniciar.getBtn(),cargar.getBtn(),regla.getBtn(),puntaje.getBtn(),salir.getBtn());
         
         
         reglas = null;
@@ -63,9 +69,11 @@ public class PaneOrganize
         
         //ACCIONES DE LOS BOTONES
         regla.getBtn().setOnAction(new ventanaReglas());
+        cargar.getBtn().setOnAction(new cargarPartida());
         salir.getBtn().setOnAction(new Salida());
         iniciar.getBtn().setOnAction(new Iniciar());
         puntaje.getBtn().setOnAction(new ventanaPuntajes());
+        
     }
     
     private class ventanaPuntajes implements EventHandler<ActionEvent>
@@ -109,7 +117,7 @@ public class PaneOrganize
         {
             if(inicioJuego == null)
             {
-                setInicioJuego(new InicioJuego(getStagePrincipal()));
+                setInicioJuego(new InicioJuego(getStagePrincipal(),datos,0));
                 getInicioJuego().getStageIngresoDatos().show();
                 stagePrincipal.hide();
             }
@@ -122,6 +130,45 @@ public class PaneOrganize
             event.consume();
         }
     }
+    private class cargarPartida implements EventHandler<ActionEvent>
+    {
+        
+        @Override
+        public void handle(ActionEvent event)     
+        {
+            
+            file= new File("Partidas Guardadas.txt");
+                try {
+                    System.out.println("sadgsadf");   
+                    fr = new FileReader(file);
+                    br = new BufferedReader(fr);
+                    try{
+                        String st = br.readLine();
+                        //while(st!= null){
+                            datos= st.split(",");
+                            System.out.println("Nombre "+datos[0]);
+                            System.out.println("Nivel "+datos[1]);
+                            System.out.println("Puntaje "+datos[2]);
+                            System.out.println("Vidas "+datos[3]);
+                            st = br.readLine();
+                        //}
+                    }catch(Exception e1){
+                        System.out.println("error");
+                    }
+                }catch (Exception e2) {
+                    System.out.println("no lee");
+                }
+
+
+                setInicioJuego(new InicioJuego(getStagePrincipal(),datos,1));
+                getInicioJuego().getStageInicioJuego().show();
+                stagePrincipal.hide();
+                  
+            event.consume();
+        }
+         
+    }
+    
 
     private class Salida implements EventHandler<ActionEvent>
     {
